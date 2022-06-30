e724480557743a435dbcc40d3c3442a6256bb7d4
이민서 <sola2014@naver.com>
2022-06-30 20:37:33 +0900

    Greedy 설탕 배달[Silver4]

---
+++ Solve.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let input = fs.readFileSync('inputFile.txt').toString().split('\n');
+// const input = fs.readFileSync("inputFile.txt").toString().trim().split(" ");
+
+sum = 0
+for(var i = 0; i <= 4; i++) {
+    if((input[0]-(3*i))%5 == 0 && input[0]-(3*i) >= 0) {
+        sum += i + ((input[0]-(3*i))/5)
+        console.log(sum)
+        break
+    }
+}
+if(sum == 0) {
+    if(input[0]%3 == 0){
+        console.log(input[0]/3)
+    } else {
+        console.log("-1")
+    }
+}
