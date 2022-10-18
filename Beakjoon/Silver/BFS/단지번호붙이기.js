061a71567e2bd7a39466711dbaeae971549a2f32
이민서 <sola2014@naver.com>
2022-10-18 19:46:27 +0900

    유기농 배추 완료

--- Beakjoon/Silver/BFS/단지번호붙이기.js
+++ Beakjoon/Silver/BFS/단지번호붙이기.js
@@ -43,12 +43,11 @@ class Queue {
 // const input = fs.readFileSync('./inputFile.txt').toString().split("\n")
 const input = fs.readFileSync('/dev/stdin').toString().split("\n")
 const N = Number(input.shift())
-input.shift()
-const board = input.map(v => v.replace(/\s*/g, "").split(""))
-const visited = Array(N).fill(false)
+const board = input.map(v => v.replace(/\s*/g, "").split("").map(v => +v))
+const visited = Array.from(Array(N), () => Array(N).fill(false))
 
-console.table(board)
-console.table(visited)
+// console.table(board)
+// console.table(visited)
 const dx = [0, 1, 0, -1]
 const dy = [1, 0, -1, 0]
 
@@ -56,7 +55,35 @@ let cnt = 0
 const resValue = []
 
 const BFS = () => {
-
+    for (i = 0 ; i < N; i++) {
+        for (j = 0 ; j < N; j ++) {
+            if (board[i][j] == 1 && !visited[i][j]) {
+                const quque = new Queue();
+                quque.offer([i, j])
+                cnt +=1
+                cntBuilt = 1 
+                while (quque.size() !== 0) {
+                    now = quque.poll()
+                    visited[i][j] = true
+                    for (k = 0; k < 4; k++) {
+                        const x = now[0] + dx[k]
+                        const y = now[1] + dy[k]
+                        if (x >= 0 && y >= 0 && x < N && y < N ) {
+                            if (!visited[x][y] && board[x][y] == 1) {
+                                visited[x][y] = true
+                                quque.offer([x, y])
+                                cntBuilt +=1
+                            }
+                        }
+                    }
+                }
+                resValue.push(cntBuilt)
+            }
+        }
+    }
 }
 
 BFS()
+console.log(cnt)
+resValue.sort((a, b) => a - b)
+resValue.map(v => console.log(v))
